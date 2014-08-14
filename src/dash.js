ce473e5808076d84ed7b161ccaeda0b66206b585
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2014-08-14 11:55:14 -0400

    Fixed malformed XML, added jQuery

--- src/dash.js
+++ src/dash.js
@@ -1,3 +1,20 @@
 "use strict";
 
 console.log("Testing!");
+
+
+$.ajax({
+
+	type: 'GET',
+	dataType: 'xml',
+	url: 'competencies/competency-frameworks/v1/demonstrate-warrior-athlete-ethos.xml',
+	success: function(data){
+	
+		console.log(data);
+		debugger;
+	},
+	error: function(err, status, mess){
+	
+		console.log("ERROR!!", err, status, mess);
+	}
+});
