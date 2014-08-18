4b67f6302473d1261aeb4e9e7d208734e4e47c2e
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2014-08-18 16:10:54 -0400

    Added initial chart for each performance level

--- src/dash.js
+++ src/dash.js
@@ -1,140 +1,188 @@
 "use strict";
-
-var userScores = localStorage.userScores ? JSON.parse(localStorage.userScores) : null;
-var perf;
-
-$.ajax({
-	type: 'GET',
-	dataType: 'xml',
-	url: 'competencies/performance-frameworks/v1/demonstrate-warrior-athlete-ethos.xml',
-	success: function(data){
+(function(window, $){
+	var userScores = localStorage.userScores ? JSON.parse(localStorage.userScores) : null;
+	var perf;
+	var colors = {gray:["rgba(220,220,220,0.5)", "rgba(220,220,220,1)"], blue:["rgba(151,187,205,0.5)", "rgba(151,187,205,1)"], purple:["rgba(201, 198, 229, 0.5)", "rgba(201, 198, 229, 1)"], green:["rgba(151, 205, 158, 0.5)","rgba(151, 205, 158, 1)"]};
+	var extraColorKeys = ["purple", "green"];
 	
-		perf = parsePerformanceFramework(data);
-		generateTestData();
-		drawChart();
+	$.ajax({
+		type: 'GET',
+		dataType: 'xml',
+		url: 'competencies/performance-frameworks/v1/demonstrate-warrior-athlete-ethos.xml',
+		success: function(data){
 		
-		perf.handleScore = handleScore;
-		ko.applyBindings(perf);
+			perf = parsePerformanceFramework(data);
+			generateTestData();
+			drawChart();
+			
+			perf.handleScore = handleScore;
+			//perf.drawHorizontalBar = drawHorizontalBar;
+			ko.applyBindings(perf);
+			
+			drawHorizontalBar();
+			
+			console.log(perf);
+			console.log(userScores);
+		},
+		error: function(err, status, mess){
+			console.error("ERROR!!", err, status, mess);
+		}
+	});
+
+	function drawChart(){
+		var sum = userScores.reduce(function(a, b){ return a + b});
+		var sumRemaining = perf.Component.reduce(function(a, b){ return (a>0?a:a.PerformanceLevelSet.total) + (b>0?b:b.PerformanceLevelSet.total)}) - sum;
+		var options = {percentageInnerCutout: 45, segmentStrokeColor : sumRemaining > 0 ? "#FFF" : "#FDB45C"};
 		
-		console.log(perf);
-		console.log(userScores);
-	},
-	error: function(err, status, mess){
-		console.error("ERROR!!", err, status, mess);
+		var data = [
+			{
+				value: sumRemaining,
+				color: "#46BFBD",
+				highlight: "#5AD3D1",
+				label: "Score Remaining"
+			},		
+			{
+				value: sum,
+				color:"#FDB45C",
+				highlight: "#FFC870",
+				label: "Your Total Score"
+			}, 
+		];
+		
+		//var chart = new Chart(document.getElementById('myChart').getContext('2d')).Doughnut(data, options);
+		//$("#legend").html(chart.generateLegend());
 	}
-});
-
-function drawChart(){
-	var sum = userScores.reduce(function(a, b){ return a + b});
-	var sumRemaining = perf.Component.reduce(function(a, b){ return (a>0?a:a.PerformanceLevelSet.total) + (b>0?b:b.PerformanceLevelSet.total)}) - sum;
-	var options = {percentageInnerCutout: 45, segmentStrokeColor : sumRemaining > 0 ? "#FFF" : "#FDB45C"};
 	
-	var data = [
-		{
-			value: sumRemaining,
-			color: "#46BFBD",
-			highlight: "#5AD3D1",
-			label: "Score Remaining"
-		},		
-		{
-			value: sum,
-			color:"#FDB45C",
-			highlight: "#FFC870",
-			label: "Your Total Score"
-		}, 
-	];
-	var chart = new Chart(document.getElementById('myChart').getContext('2d')).Doughnut(data, options);
-	$("#legend").html(chart.generateLegend());
-}
-
-function handleScore(single, index){
-	//console.log(single, index);
-	
-	var score = userScores[index], str = "This is where you are!";
-	if(single.include == "both" && single.min <= score && score <= single.max){
-		return str;
-	}
-	else if(single.include == "min" && single.min <= score && score < single.max){
-		return str;
-	}
-	else if(single.include == "max" && single.min < score && score <= single.max){
-		return str;
-	}
-
-	return "";
-}
-
-function generateTestData(){
-	if(!userScores){
-		userScores = [];
+	function drawHorizontalBar(){
 		for(var i = 0; i < perf.Component.length; i++){
-			userScores.push(Math.round(Math.random() * perf.Component[i].PerformanceLevelSet.total));
+			var data = {labels: [""], datasets: []};
+			var perfLevel = perf.Component[i].PerformanceLevelSet;
+			var totalDiff = userScores[i];
+
+			data.datasets.push({
+				fillColor : colors.blue[0],
+				strokeColor : colors.blue[1],
+				data: [userScores[i]],
+				title: "Current"
+			});
+			
+			for(var g = 0; g < perfLevel.PerformanceLevel.length-1; g++){
+				if(userScores[i] < perfLevel.PerformanceLevel[g].max){
+					if(!Array.isArray(perfLevel.PerformanceLevel[g].include.match(/both|max/))){
+						perfLevel.PerformanceLevel[g].max--;
+					}
+					
+					data.datasets.push({
+						fillColor : colors[extraColorKeys[g]][0],
+						strokeColor : colors[extraColorKeys[g]][1],
+						data: [perfLevel.PerformanceLevel[g].max-totalDiff],
+						title: "Current"
+					});
+					
+					totalDiff = perfLevel.PerformanceLevel[g].max;
+				}
+			}
+			
+			data.datasets.push({
+			    fillColor : colors.gray[0],
+				strokeColor : colors.gray[1],
+				data: [perfLevel.total-totalDiff],
+				title: "Goal"
+			});
+
+			var chart = new ChartNew(document.getElementById('canvas' + i).getContext('2d')).HorizontalStackedBar(data, {graphMin: 0});
 		}
-		localStorage.userScores = JSON.stringify(userScores);
 	}
-}
 
-function parsePerformanceFramework(xml){
-
-	var jXML = $(xml);
-	
-	var outObj;
-	try{
-		outObj = JSON.parse(xml2json(xml).replace("undefined", "")).PerformanceFramework;
-	}
-	catch(e){
-		console.error("Unable to parse XML document", xml, e);
-		return;
-	}
-	
-	for(var i = 0; i < outObj.Component.length; i++){
-		var performanceLevelArr = outObj.Component[i].PerformanceLevelSet.PerformanceLevel;
+	function handleScore(single, index){
+		//console.log(single, index);
 		
-		for(var g = 0; g < performanceLevelArr.length; g++){
-			parsePerformanceStr(performanceLevelArr[g]);
+		var score = userScores[index], str = "This is where you are!";
+		if(single.include == "both" && single.min <= score && score <= single.max){
+			return str;
 		}
-		
-		outObj.Component[i].PerformanceLevelSet.total = performanceLevelArr[0].total;
-	}
-	
-	return outObj;
-}
-
-function parsePerformanceStr(perfObj){
-
-	var intervalArr, str = perfObj.Indicator['@id'];
-
-	if(str.indexOf('between') > -1){
-		intervalArr = str.match(/([0-9]+)-and-([0-9]+)-out-of-([0-9]+)/);
-		perfObj.total = +intervalArr[3];
-		
-		perfObj.min = +intervalArr[1];
-		perfObj.max = +intervalArr[2];
-		perfObj.include = "both";
-	}
-	
-	else{
-		intervalArr = str.match(/([0-9]+)-out-of-([0-9]+)/);
-		perfObj.total = +intervalArr[2];
-		
-		if(str.indexOf('less') > -1){
-			perfObj.min = 0;
-			perfObj.max = +intervalArr[1];
-			perfObj.include = "min";
-		}	
-		else if(str.indexOf('more') > -1){
-			perfObj.min = +intervalArr[1];
-			perfObj.max = +intervalArr[2];
-			perfObj.include = "max";
-		}	
-		else if(intervalArr){
-			perfObj.min = +intervalArr[1];
-			perfObj.max = +intervalArr[1];
-			perfObj.include = "both";
+		else if(single.include == "min" && single.min <= score && score < single.max){
+			return str;
 		}
-		else{
-			console.error("Unable to parse performance string: ", str);
+		else if(single.include == "max" && single.min < score && score <= single.max){
+			return str;
+		}
+
+		return "";
+	}
+
+	function generateTestData(){
+		if(!userScores){
+			userScores = [];
+			for(var i = 0; i < perf.Component.length; i++){
+				userScores.push(Math.round(Math.random() * perf.Component[i].PerformanceLevelSet.total));
+			}
+			localStorage.userScores = JSON.stringify(userScores);
+		}
+	}
+
+	function parsePerformanceFramework(xml){
+
+		var jXML = $(xml);
+		
+		var outObj;
+		try{
+			outObj = JSON.parse(xml2json(xml).replace("undefined", "")).PerformanceFramework;
+		}
+		catch(e){
+			console.error("Unable to parse XML document", xml, e);
 			return;
 		}
+		
+		for(var i = 0; i < outObj.Component.length; i++){
+			var performanceLevelArr = outObj.Component[i].PerformanceLevelSet.PerformanceLevel;
+			
+			for(var g = 0; g < performanceLevelArr.length; g++){
+				parsePerformanceStr(performanceLevelArr[g]);
+			}
+			
+			outObj.Component[i].PerformanceLevelSet.total = performanceLevelArr[0].total;
+		}
+		
+		return outObj;
 	}
-}
+
+	function parsePerformanceStr(perfObj){
+
+		var intervalArr, str = perfObj.Indicator['@id'];
+
+		if(str.indexOf('between') > -1){
+			intervalArr = str.match(/([0-9]+)-and-([0-9]+)-out-of-([0-9]+)/);
+			perfObj.total = +intervalArr[3];
+			
+			perfObj.min = +intervalArr[1];
+			perfObj.max = +intervalArr[2];
+			perfObj.include = "both";
+		}
+		
+		else{
+			intervalArr = str.match(/([0-9]+)-out-of-([0-9]+)/);
+			perfObj.total = +intervalArr[2];
+			
+			if(str.indexOf('less') > -1){
+				perfObj.min = 0;
+				perfObj.max = +intervalArr[1];
+				perfObj.include = "min";
+			}	
+			else if(str.indexOf('more') > -1){
+				perfObj.min = +intervalArr[1];
+				perfObj.max = +intervalArr[2];
+				perfObj.include = "max";
+			}	
+			else if(intervalArr){
+				perfObj.min = +intervalArr[1];
+				perfObj.max = +intervalArr[1];
+				perfObj.include = "both";
+			}
+			else{
+				console.error("Unable to parse performance string: ", str);
+				return;
+			}
+		}
+	}
+}(window, jQuery));
