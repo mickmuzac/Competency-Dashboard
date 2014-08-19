39be849c353bddc1ffa51581d6e9bb44a88f278b
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2014-08-19 15:29:07 -0400

    Added more granular data to pie chart

--- src/dash.js
+++ src/dash.js
@@ -1,6 +1,8 @@
 "use strict";
 (function(window, $){
 	var userScores = localStorage.userScores ? JSON.parse(localStorage.userScores) : null;
+	var tempScores = [];
+	
 	var perf;
 	var colors = {
 		gray:["rgba(220,220,220,0.5)", "rgba(220,220,220,1)"], 
@@ -11,7 +13,7 @@
 		red: ["rgba(237, 65, 65, 0.5)", "#DB7D7D"],
 		darkgray: ["rgba(190, 190, 190, 0.5)","#ccc"]
 	};
-	var extraColorKeys = ["green", "blue"];
+	var extraColorKeys = ["green", "blue", "gold", "red", "purple"];
 	
 	$.ajax({
 		type: 'GET',
@@ -20,16 +22,25 @@
 		success: function(data){
 		
 			perf = parsePerformanceFramework(data);
+			
+			var tempArr = [];
+			for(var i = 0; i < perf.Component.length; i++){
+				for(var g = 0; g < perf.Component[i].PerformanceLevelSet.PerformanceLevel.length; g++){
+					tempArr.push(perf.Component[i].PerformanceLevelSet.PerformanceLevel[g]);
+				}
+			}
+			
 			generateTestData();
-			drawChart();
 			
 			perf.getImgURL = getImgURL;
 			perf.isCurrentScore = isCurrentScore;
-			//perf.drawHorizontalBar = drawHorizontalBar;
 			ko.applyBindings(perf);
 			
-			drawHorizontalBar();
+			var collection = (new ADL.Collection(tempArr)).groupBy('Indicator.Description');
 			
+			drawHorizontalBar();
+			drawChart(collection);
+
 			console.log(perf);
 			console.log(userScores);
 		},
@@ -38,28 +49,42 @@
 		}
 	});
 
-	function drawChart(){
+	function drawChart(collection){
 		var sum = userScores.reduce(function(a, b){ return a + b});
 		var sumRemaining = perf.Component.reduce(function(a, b){ return (a>0?a:a.PerformanceLevelSet.total) + (b>0?b:b.PerformanceLevelSet.total)}) - sum;
 		var options = {percentageInnerCutout: 45, segmentStrokeColor : sumRemaining > 0 ? "#FFF" : "#FDB45C"};
 		
-		var data = [
-			{
-				value: sumRemaining,
-				color: "#46BFBD",
-				highlight: "#5AD3D1",
-				label: "Score Remaining"
-			},		
-			{
-				value: sum,
-				color:"#FDB45C",
-				highlight: "#FFC870",
-				label: "Your Total Score"
-			}, 
-		];
+		//Copy userScores so we don't mutate the original values
+		var data = [];
 		
-		//var chart = new Chart(document.getElementById('myChart').getContext('2d')).Doughnut(data, options);
-		//$("#legend").html(chart.generateLegend());
+		data.push({
+			value: sum,
+			color:colors.darkgray[0],
+			highlight: colors.darkgray[1],
+			label: "Your Total Score"
+		});
+		
+		for(var i = 0; i < collection.contents.length; i++){
+			var totalCount = 0;
+			for(var g = 0; g < collection.contents[i].data.length; g++){
+
+				var item =  collection.contents[i].data[g];
+				
+				if(item._pie != "over"){
+					totalCount += item._pointValue;
+				}
+			}
+
+			data.push({
+				value: totalCount,
+				color: colors[extraColorKeys[i]][0],
+				highlight: colors[extraColorKeys[i]][1],
+				label: collection.contents[i].group
+			});	
+		}
+		
+		var chart = new Chart(document.getElementById('myChart').getContext('2d')).Doughnut(data, options);
+		$("#legend").html(chart.generateLegend());
 	}
 	
 	function drawHorizontalBar(){
@@ -69,7 +94,6 @@
 			var perfLevel = perf.Component[i].PerformanceLevelSet;
 			var totalDiff = 0;
 			
-
 			data.datasets.push({
 				fillColor : colors.darkgray[0],
 				strokeColor : colors.darkgray[1],
@@ -78,26 +102,33 @@
 			});
 			
 			for(var g = 0; g < perfLevel.PerformanceLevel.length-1; g++){
-				if(!Array.isArray(perfLevel.PerformanceLevel[g].include.match(/both|max/))){
+				if(!Array.isArray(perfLevel.PerformanceLevel[g]._include.match(/both|max/))){
 					perfLevel.PerformanceLevel[g].max--;
 				}
+				
 				var color = perfLevel.PerformanceLevel[g].Indicator.Description.toLowerCase() == "not competent" ? colors.gray : colors[extraColorKeys[g]];
+				perfLevel.PerformanceLevel[g]._pointValue = perfLevel.PerformanceLevel[g].max-totalDiff;
+				
 				data.datasets.push({
 					fillColor : color[0],
 					strokeColor : color[1],
-					data: [perfLevel.PerformanceLevel[g].max-totalDiff],
+					data: [perfLevel.PerformanceLevel[g]._pointValue],
 					title: perfLevel.PerformanceLevel[g].Indicator.Description,
 				});
 				
 				totalDiff = perfLevel.PerformanceLevel[g].max;
+				
+				handlePieChartInfo(perfLevel.PerformanceLevel[g], i);
 			}
 			
+			perfLevel.PerformanceLevel[g]._pointValue = perfLevel.total-totalDiff;
 			data.datasets.push({
 			    fillColor : colors.gold[0],
 				strokeColor : colors.gold[1],
 				data: [perfLevel.total-totalDiff],
 				title: perfLevel.PerformanceLevel[g].Indicator.Description
 			});
+			handlePieChartInfo(perfLevel.PerformanceLevel[g], i);
 
 			var options = {
 				annotateLabel: "<%=(v1 == '' ? '' : v1) %>",
@@ -108,16 +139,41 @@
 		}
 	}
 	
+	function handlePieChartInfo(perf, index){
+		if(perf._pie == "over"){
+			tempScores[index] -= perf._pointValue;
+			perf._pointValue = 0;
+		}		
+		else if(perf._pie == "current"){
+			
+			perf._pointValue -= tempScores[index];
+			tempScores[index] = 0;
+		}		
+	}
+	
 	function isCurrentScore(single, index){
 
 		var score = userScores[index];
-		if(single.include == "both" && single.min <= score && score <= single.max){
+		
+		if(((single._include == "both" || single._include == "max") && score > single.max) || (single._include == "min" && score >= single.max)){
+			single._pie = "over";
+			return false;
+		}
+		else if(((single._include == "both" || single._include == "min") && score < single.min) || (single._include == "max" && score <= single.min)){
+			single._pie = "under";
+			return false;
+		}
+		
+		if(single._include == "both" && single.min <= score && score <= single.max){
+			single._pie = "current";
 			return true;
 		}
-		else if(single.include == "min" && single.min <= score && score < single.max){
+		else if(single._include == "min" && single.min <= score && score < single.max){
+			single._pie = "current";
 			return true;
 		}
-		else if(single.include == "max" && single.min < score && score <= single.max){
+		else if(single._include == "max" && single.min < score && score <= single.max){
+			single._pie = "current";
 			return true;
 		}
 
@@ -125,6 +181,7 @@
 	}		
 	
 	function getImgURL(single){
+		return "";
 		var lower = single.Indicator.Description.toLowerCase(), 
 			lowerMatch = lower.match(/advanced|intermediate|beginner/),
 			prefix = "img/";
@@ -150,6 +207,8 @@
 			}
 			localStorage.userScores = JSON.stringify(userScores);
 		}
+
+		Array.prototype.push.apply(tempScores, userScores);
 	}
 
 	function parsePerformanceFramework(xml){
@@ -188,7 +247,7 @@
 			
 			perfObj.min = +intervalArr[1];
 			perfObj.max = +intervalArr[2];
-			perfObj.include = "both";
+			perfObj._include = "both";
 		}
 		
 		else{
@@ -198,17 +257,17 @@
 			if(str.indexOf('less') > -1){
 				perfObj.min = 0;
 				perfObj.max = +intervalArr[1];
-				perfObj.include = "min";
+				perfObj._include = "min";
 			}	
 			else if(str.indexOf('more') > -1){
 				perfObj.min = +intervalArr[1];
 				perfObj.max = +intervalArr[2];
-				perfObj.include = "max";
+				perfObj._include = "max";
 			}	
 			else if(intervalArr){
 				perfObj.min = +intervalArr[1];
 				perfObj.max = +intervalArr[1];
-				perfObj.include = "both";
+				perfObj._include = "both";
 			}
 			else{
 				console.error("Unable to parse performance string: ", str);
