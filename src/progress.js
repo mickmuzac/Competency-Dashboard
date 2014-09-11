d04bdc29d82af1e646bb616a564c9d455ce034ce
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2014-09-11 09:19:32 -0400

    Click handler, css cleanup
    Changed click handler to increment/decrement instead of randomize
    Now calculates overall progress based on components
    Changed badge aspect ratio, cleaned up css

--- src/progress.js
+++ src/progress.js
@@ -3,31 +3,51 @@ var app = angular.module('badge-progress', []);
 app.controller('BadgeCtrl', ['$scope', function($scope)
 {
 	$scope.children = [
-		{name: 'Strength', color: 'orange', image: 'img/Strength_Badge-Large.png'},
-		{name: 'Endurance', color: 'firebrick', image: 'img/Endurance_Badge-Large.png'},
-		{name: 'Mobility', color: 'orangered', image: 'img/Mobility_Badge-Large.png'},
-		{name: 'Nutrition', color: 'limegreen', image: 'img/Nutrition_Badge-Large.png'},
-		{name: 'Ethos', color: 'dodgerblue', image: 'img/Philosophy_Badge-Large.png'}
+		{name: 'Strength', color: 'orange', image: 'img/Strength_Badge-Large.png', progress: 0},
+		{name: 'Endurance', color: 'firebrick', image: 'img/Endurance_Badge-Large.png', progress: 0},
+		{name: 'Mobility', color: 'orangered', image: 'img/Mobility_Badge-Large.png', progress: 0},
+		{name: 'Nutrition', color: 'limegreen', image: 'img/Nutrition_Badge-Large.png', progress: 0},
+		{name: 'Ethos', color: 'dodgerblue', image: 'img/Philosophy_Badge-Large.png', progress: 0}
 	];
 
 	$scope.name = 'Warrior Athlete';
 	$scope.color = 'purple';
 	$scope.image = 'img/Soldier-Athlete_Badge-Large.png';
-	$scope.progress = Math.floor(Math.random()*100);
+	$scope.progress = 0;
 
-	$scope.genGradient = function(color){
+	$scope.$watchGroup(['children[0].progress','children[1].progress','children[2].progress','children[3].progress','children[4].progress'],
+		function(newVals, oldVals){
+			$scope.progress = newVals.reduce(function(old,cur){return old+cur;}, 0) / 5;
+		}
+	);
+
+	$scope.genGradient = function(comp){
+		var bgColor = 'rgba(255,255,255,0.6)';
+		var transition = 0;
 		return {
 			'background-image':
-				'linear-gradient( 0deg, {c}, {c} {p}%, rgba(255,255,255,0.6) {p}%, rgba(255,255,255,0.6) )'
-				.replace(/\{c\}/g, color).replace(/\{p\}/g, $scope.progress)
+				'linear-gradient( 0deg, {c}, {c} {p-}, {bg} {p+}, {bg} )'
+				.replace(/\{c\}/g, comp.color)
+				.replace(/\{p-\}/g, (comp.progress-transition)+'%')
+				.replace(/\{p\+\}/g, (comp.progress+transition)+'%')
+				.replace(/\{bg\}/g, bgColor)
 		};
 	};
 
-	$scope.randomizeValue = function(evt){
-		$scope.progress = Math.floor(Math.random()*100);
+	$scope.incrementValue = function(comp, evt)
+	{
+		if(evt.button === 0)
+			comp.progress = Math.min(comp.progress+20, 100);
+		else
+			comp.progress = Math.max(comp.progress-20, 0);
+
 		evt.stopPropagation();
-		return false;
 	};
 
 }]);
+
+var noCM = function(e){
+	if(e.preventDefault) e.preventDefault();
+	return false;
+};
 
